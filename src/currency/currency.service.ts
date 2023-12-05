16947e483404a8fcd313feadb2a1118b8db338f2
Denis Kurdesov <scorpionbeat@gmail.com>
2023-12-05 15:39:18 +0900

    Add currency rates fetching from API

--- src/currency/currency.service.ts
+++ src/currency/currency.service.ts
@@ -1,33 +1,49 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { map, lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class CurrencyService {
-    
-    private readonly exchangeRates: { [from: string]: { [to: string]: number } } = {
-        'JPY': {
-            'JPY': 1
-        },
-        'USD': {
-            'JPY': 147.16
-        },
-        'EUR': {
-            'JPY': 159.45
-        },
-        'GBP': {
-            'JPY': 185.87
-        },
-        'PLN': {
-            'JPY': 36.80 
-        },
-    }
-    
-    async convertToJPY(amount: number, currency: string): Promise<number> {
-        currency = currency.toUpperCase()
+  constructor(private readonly httpService: HttpService) {}
 
-        if (!this.exchangeRates[currency]){
-            throw new NotFoundException(`Unknown currency "${currency}".`)
-        }
-        
-        return this.exchangeRates[currency]['JPY'] * amount
+  private rates: { [currency: string]: number };
+
+  async convertToJPY(amount: number, currency: string): Promise<number> {
+    currency = currency.toUpperCase();
+
+    if (currency === 'JPY') {
+      return amount;
     }
+
+    if (!this.rates) {
+        await this.fetchRates()
+    }
+
+    if (!(currency in this.rates)) {
+      throw new BadRequestException(`Unknown currency "${currency}".`);
+    }
+
+    const convertedAmount = (amount * this.rates['JPY']) / this.rates[currency];
+
+    return convertedAmount;
+  }
+
+  private async fetchRates(): Promise<void> {
+    const result = await lastValueFrom(
+      this.httpService
+        .get(`${process.env.CURRENCY_RATES_API}/latest`)
+        .pipe(map((response) => response.data)),
+    );
+
+    if (!result || !result.rates) {
+      throw new Error('Cannot fetch currencies.');
+    }
+
+    this.rates = result.rates;
+    this.rates['EUR'] = 1;
+  }
 }
